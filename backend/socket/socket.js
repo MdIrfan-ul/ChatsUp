8835d2e79cacf40bcd619816c2a453ceec58cdc4
Mohamed Irfanullah M <mohamedirfan1720@gmail.com>
2024-08-28 00:26:24 +0530

    socket updated

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["https://main--chats-up.netlify.app"],
+		origin: ["https://chats-up.netlify.app"],
 		methods: ["GET", "POST"],
 		credentials:true
 	},
